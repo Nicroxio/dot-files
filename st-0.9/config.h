0412239b21e042f305a5db7eeec43c66e1225c6b
Nicroxio <ferdi@beacroft-mitchell.co.uk>
2022-12-28 22:02:20 +0000

    change

--- st-0.9/config.h
+++ st-0.9/config.h
@@ -163,8 +163,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 40;
-static unsigned int rows = 12;
+static unsigned int cols = 80;
+static unsigned int rows = 24;
 
 /*
  * Default colour and shape of the mouse cursor
